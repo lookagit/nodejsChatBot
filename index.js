7477108f17e5b67da0e986599e1114775a9898eb
Luka-Kvant <simjanovic.luka@gmail.com>
2017-03-10 21:47:32 +0100

    blob

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook/', function (req, res) {
   var data = req.body;
-  console.log(data)
+  console.log(req)
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
