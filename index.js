4c9868e37f308faadd70db0b67771d5a73e36b1c
Luka-Kvant <simjanovic.luka@gmail.com>
2017-03-10 21:34:06 +0100

    consoled

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook/', function (req, res) {
   var data = req.body;
-
+  console.log("EVO MEEEEEEEEEEEEEEE")
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
