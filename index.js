93513597d9268748df7f03d1bd2e156742ada04b
Luka-Kvant <simjanovic.luka@gmail.com>
2017-03-11 19:25:45 +0100

    minor

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var bodyParser = require('body-parser');
-var receivedMessage = require('./chatBotFunction')
+var receivedMessage = require('./chatBotFunction');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
